d7c38f42f8e2deb83682e63291c20a12e92604a8
Joaquin Telleria <joaquintelleria@gmail.com>
2024-04-11 18:17:27 +0200

    add rainbowkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
 
 import './App.css'
 import { Button } from "@/components/ui/button"
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  ConnectButton,
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+  appName: 'project-opus',
+  projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+  ssr: false,
+});
+
+const queryClient = new QueryClient();
+
 
 function App() {
 
   return (
     <>
-      <Button>Click me</Button>
-
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider>
+            <Button> Hi! </Button>
+            <ConnectButton />
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
     </>
   )
 }
